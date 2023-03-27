ec9a35aaf5f90d5617df0bcdbbcca882d84b99fa
TeselkinaMaryia <mshts00@mail.ru>
2023-03-27 22:49:42 +0300

    add tests.py for Contact model

--- business_website/about/tests.py
+++ business_website/about/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from . import models
 
-# Create your tests here.
+
+class TestCantactCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        models.Cantact.objects.create(name='Sara',
+                                      email='saraTerner@mail.ru',
+                                      subject='Work in the company.',
+                                      message='I\'d like to work in this company')
+
+    def test_get(self):
+        self.assertEqual(str(models.Cantact.objects.get(id=1)), 'saraTerner@mail.ru')
